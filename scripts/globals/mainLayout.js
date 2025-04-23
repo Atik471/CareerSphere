5c5363144aabe394f4a37c68ac94a415e328f413
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-04-23 17:11:41 +0600

    added navbar

--- scripts/globals/mainLayout.js
+++ scripts/globals/mainLayout.js
@@ -1,12 +1,24 @@
-fetch("../../views/globals/sidebar/sidebar.html")
-  .then((response) => response.text())
-  .then((html) => {
-    document.getElementById("sidebar").innerHTML = html;
-    requestAnimationFrame(() => {
+const loadLayout = () => {
+  fetch("../../views/globals/sidebar/sidebar.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("sidebar").innerHTML = html;
+      requestAnimationFrame(() => {
         initSidebar();
       });
-    
-  })
-  .catch((error) => {
-    console.error("Error loading dashboard:", error);
-  });
+    })
+    .catch((error) => {
+      console.error("Error loading sidebar:", error);
+    });
+
+  fetch("../../views/globals/navbar/navbar.html")
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("navbar").innerHTML = html;
+    })
+    .catch((error) => {
+      console.error("Error loading navbar:", error);
+    });
+};
+
+loadLayout();
--- scripts/globals/sidebar.js
+++ scripts/globals/sidebar.js
@@ -3,6 +3,8 @@ function initSidebar() {
     const sidebar = document.getElementById("sidebar");
     const toggleBtn = document.getElementById("toggle-btn");
     const main = document.getElementById("main");
+    const sidebarLogo = document.getElementById("sidebar-logo");
+
 
     const isCollapsed = sidebar.classList.contains("collapsed");
 
@@ -10,11 +12,19 @@ function initSidebar() {
       sidebar.classList.remove("collapsed");
       sidebar.classList.add("expanded");
       main.style.marginLeft = 250 + "px";
+      sidebarLogo.style.display = "block";
     } else {
       sidebar.classList.remove("expanded");
       sidebar.classList.add("collapsed");
-      main.style.marginLeft = 100 + "px";
+      main.style.marginLeft = 75 + "px";
+      sidebarLogo.style.display = "none";
     }
+
+    const listItems = document.querySelectorAll("#sidebar li span");
+    listItems.forEach((span) => {
+      span.classList.toggle("hide-text");
+    });
+    
   };
 
   const toggleBtn = document.getElementById("toggle-btn");
